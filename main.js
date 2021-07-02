a09851541166fb90a57d8f4754f63dacb7252f25
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-07-02 14:27:54 +0300

    add show function

---
+++ main.js
@@ -0,0 +1,7 @@
+var dropdown = document.getElementById('d');
+var options = document.getElementById('o');
+var input = document.getElementById('v');
+
+dropdown.addEventListener('click', function() {
+    this.classList.toggle('show')
+})
