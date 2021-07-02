2ddf9b20a5b97dbc72611952a8896bf5a9c163b5
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-07-02 14:35:04 +0300

    getting value of choosen element

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ var input = document.getElementById('v');
 dropdown.addEventListener('click', function() {
     this.classList.toggle('show')
 })
+
+// for getting value of choosen element
+options.addEventListener('click', function(event) {
+    const listItem = event.target;
+    console.log(listItem);
+    const value = listItem.attributes.cName.value;
+    console.log(value);
+})
